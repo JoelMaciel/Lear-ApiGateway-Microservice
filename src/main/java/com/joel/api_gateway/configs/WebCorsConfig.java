d5f62d9bfa6f38fe9df09a4c0b58cd55188ba553
Joel <joel.viana@audora.com.br>
2024-06-29 15:39:35 -0300

    feat: implementing cors in  application

---
+++ src/main/java/com/joel/api_gateway/configs/WebCorsConfig.java
@@ -0,0 +1,26 @@
+package com.joel.api_gateway.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebCorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200"); // Permite apenas essa origem
+        config.addAllowedHeader("*"); // Permite todos os cabeçalhos
+        config.addAllowedMethod("*"); // Permite todos os métodos
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
